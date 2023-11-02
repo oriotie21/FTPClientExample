d3bbf49e3c709aa035a450e7a619e5db8a233247
hoony99 <hoony0928@sju.ac.kr>
2023-11-03 01:47:33 +0900

    server & login 연동
    server & login 연동

--- src/App.java
+++ src/App.java
@@ -3,8 +3,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
+import javax.swing.JTextArea;
 
 
 public class App {
@@ -15,49 +17,101 @@ public class App {
 
     static void guiMain(){
         
-            JFrame frame = new JFrame("FTP program");
-            JPanel panel = new JPanel();
-            JButton upload = new JButton("Upload");
-            JButton download = new JButton("Download");
-    
-            frame.add(panel);
-            panel.add(upload);
-            panel.add(download);
-    
-            // Add action listener for the upload button
-            upload.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JFrame upFrame = new JFrame("Upload");
-                    upFrame.add(new UploadPanel());
-    
+		JFrame loginFrame = new JFrame("Welcome to FTP Program");
+		JPanel loginPanel = new JPanel();
+
+		JLabel host = new JLabel("Host:");
+		JTextArea hostText = new JTextArea(1, 100);
+
+		JLabel port = new JLabel("Port:");
+		JTextArea portText = new JTextArea(1, 100);
+
+		JLabel user = new JLabel("Username:");
+		JTextArea userText = new JTextArea(1, 100);
+
+		JLabel pw = new JLabel("Password:");
+		JTextArea pwText = new JTextArea(1, 100);
+
+		JButton loginBtn = new JButton("Login");
+
+		loginFrame.add(loginPanel);
+		loginPanel.setLayout(null);
+
+		host.setBounds(10, 10, 50, 20); // Set bounds for label
+		hostText.setBounds(100, 10, 265, 20); // Set bounds for text area
+
+		port.setBounds(10, 40, 50, 20);
+		portText.setBounds(100, 40, 265, 20);
+
+		user.setBounds(10, 70, 80, 20);
+		userText.setBounds(100, 70, 265, 20);
+
+		pw.setBounds(10, 100, 70, 20);
+		pwText.setBounds(100, 100, 265, 20);
+
+		loginBtn.setBounds(290, 130, 80, 20);
+
+		loginPanel.add(host);
+		loginPanel.add(hostText);
+
+		loginPanel.add(port);
+		loginPanel.add(portText);
+
+		loginPanel.add(user);
+		loginPanel.add(userText);
+
+		loginPanel.add(pw);
+		loginPanel.add(pwText);
+
+		loginPanel.add(loginBtn);
+
+		loginBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				// TODO Auto-generated method stub
+				String host = hostText.getText();
+				String port = portText.getText();
+				String user = userText.getText();
+				String pw = pwText.getText();
+                int checkLogin = test(host,port,user,pw);                
+                
+
+				// 로그인 실패시 에러 메세지 후 텍스트창 비우기
+                if(checkLogin == 0){
+                    JOptionPane.showMessageDialog(null, "Login Fail", "Login Fail", JOptionPane.ERROR_MESSAGE);
+                    hostText.setText(null);
+                    portText.setText(null);
+                    userText.setText(null);
+                    pwText.setText(null);
                 }
 
-            });
-    
-            // Add action listener for the download button
-            download.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JFrame downFrame = new JFrame("Download");
-                    downFrame.add(new DownloadPanel());
-    
-                }
-            });
-    
-            frame.setVisible(true);
-            frame.setSize(300, 100);
-            frame.setLocationRelativeTo(null);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				// 로그인 성공시
+				// 로그인 성공시 서버 접속 함수 불러오기
+				// connectToServer();
+
+				// 로그인 성공시 uploadFileGUI 호출
+				JFrame selectUpDownload = new JFrame("Select Load");
+				selectUpDownload.add(new selectUpDownloadGUI());
+			}
+		});
+
+		loginFrame.setVisible(true);
+		loginFrame.setSize(400, 200);
+		loginFrame.setLocationRelativeTo(null);
+		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
-    static void test(){
-
+    
+    static int test(String host, String sport, String userid, String pw){
+        // string으로 받아온 sport번호를 integer로 형변환
+        int port = Integer.parseInt(sport);
         //연결 수립
-        FTPSession session = new FTPSession("172.22.71.20", 21, new ErrorCallback() {
+        FTPSession session = new FTPSession(host, port, new ErrorCallback() {
+        //FTPSession session = new FTPSession("172.22.71.20", 21, new ErrorCallback() {
         //FTPSession session = new FTPSession("172.18.3.85", 21, new ErrorCallback() {
             public void onError(Exception e){
-                System.out.println("!!Error!!");
+                //System.out.println("!!Error!!");
                 //System.out.println(e.getMessage());
             }
         }, new FileEventListener() {
@@ -76,39 +130,38 @@ public class App {
         });
         boolean conn = session.connect();
         if(conn){
-        //로그인
-        session.login("oriotie", "1234567");
-        //session.login("ftpuser", "ftp");
-        //CWD
-        //String cwd = session.cwd("~/aaaaaa");
-        //String cwd = session.cwd("");
-        //다운로드
-        //session.retrieveFile("sc3.png", null);
-        //session.retrieveFile("pochacco.png", null);
-        //업로드
-        //session.store("dogs.jpg", null);
-        //session.store("pochacco.png", null);
+            //로그인
+            session.login(userid, pw);
+            //session.login("ftpuser", "ftp");
+            //CWD
+            //String cwd = session.cwd("~/aaaaaa");
+            //String cwd = session.cwd("");
+            //다운로드
+            //session.retrieveFile("sc3.png", null);
+            //session.retrieveFile("pochacco.png", null);
+            //업로드
+            //session.store("dogs.jpg", null);
+            //session.store("pochacco.png", null);
 
-        //진행도 알려주는 기능 -> FileEventListener
-        //파일 목록 보여주는거(ls와 유사, 파일크기도 구해야함)
-        session.nlst(null);
-        //에러처리(로그인, 경로, 권한, 등등 + 파일 다운받다가 중단됐을때)
-        //암호화 요구 시 예외처리(표준에는 없어서 구현 안할예정)
-        //익명계정 로그인(교수님 테스트 대비)
-
-
-
-        //QUIT
-        session.quit();
+            //진행도 알려주는 기능 -> FileEventListener
+            //파일 목록 보여주는거(ls와 유사, 파일크기도 구해야함)
+            session.nlst(null);
+            //에러처리(로그인, 경로, 권한, 등등 + 파일 다운받다가 중단됐을때)
+            //암호화 요구 시 예외처리(표준에는 없어서 구현 안할예정)
+            //익명계정 로그인(교수님 테스트 대비)
+            //QUIT
+            session.quit();
+            return 1;
         }else{
             System.out.println("conn failed");
+            return 0;
         }
 
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         guiMain();
-        test();
+        //test();
     }
 
 
